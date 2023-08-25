397b64d7105dac848a4db9d3e25be4e1ae9118c8
matCzech <czechmateusz88@gmail.com>
2023-08-25 11:46:36 +0200

    custom logo on login page

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -39,3 +39,34 @@ function custom_media_button_script() {
 }
 add_action( 'admin_footer', 'custom_media_button_script' );
 
+
+//custom logo in login page
+function custom_login_logo() {
+    echo '<style type="text/css">
+        h1 a { 
+            background-image:url('.get_bloginfo('template_directory').'/twoje-logo.png) !important; 
+            background-size: contain !important; 
+            width: 100% !important; 
+            height: auto !important; 
+        }
+        .login h1 a {
+            background-color: #fff !important; // Domyślny kolor tła
+            padding-bottom: 10px;
+        }
+    </style>';
+}
+add_action('login_head', 'custom_login_logo');
+
+
+function custom_loginlogo_url($url) {
+    return get_home_url();
+}
+add_filter('login_headerurl', 'custom_loginlogo_url');
+
+
+function custom_loginlogo_title($title) {
+    return get_bloginfo('name');
+}
+add_filter('login_headertext', 'custom_loginlogo_title');
+
+
