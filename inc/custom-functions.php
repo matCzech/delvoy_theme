aa4df86a0f8aafa6ca316b7288100752a66fdc18
matCzech <czechmateusz88@gmail.com>
2023-09-01 21:08:32 +0200

    svg upload

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -70,3 +70,9 @@ function custom_loginlogo_title($title) {
 add_filter('login_headertext', 'custom_loginlogo_title');
 
 
+//SVG upload
+function add_svg_images(){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'add_svg_images');
